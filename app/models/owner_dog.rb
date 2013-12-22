06f0b298fd08b2e77e3f5a334ffadd9fc978bbcc
macheteshuffle <jeff.li029@gmail.com>
2013-12-21 20:31:38 -0500

    created Owner_Dog model and passed validation testing

--- app/models/owner_dog.rb
+++ app/models/owner_dog.rb
@@ -1,2 +1,4 @@
 class OwnerDog < ActiveRecord::Base
+  validates_presence_of :owner_id
+  validates_presence_of :dog_id
 end
--- spec/models/owner_dog_spec.rb
+++ spec/models/owner_dog_spec.rb
@@ -1,5 +1,12 @@
 require 'spec_helper'
 
 describe OwnerDog do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:owner_ids) { [1,2,3] }
+  let(:dog_ids)   { [1,2,3] }
+
+  it { should have_valid(:owner_id).when(owner_ids.include?(:owner_id)) }
+  it { should_not have_valid(:owner_id).when(nil) }
+  it { should have_valid(:dog_id).when(dog_ids.include?(:dog_id)) }
+  it { should_not have_valid(:dog_id).when(nil) }
+
 end
